8a712c5844999831b2719a9c45d0e85d166ff8b6
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-12 00:27:12 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+  def initialize(title) #initialize book with title
+    @title = title
+  end
 
+  def turn_page #method turning page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
